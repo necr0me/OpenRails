ecadbf4c06707a6e6c8ac8e6c187170ca5764d5e
pavel <ortusprogressor@gmail.com>
2022-05-03 12:58:41 +0300

    Fixed station.destroy connections issue by writing before_create method.

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,7 +1,9 @@
 class Station < ApplicationRecord
-  has_many :station_connections, dependent: :destroy
+  before_destroy :destroy_connections
+
+  has_many :station_connections
   has_many :connected_stations, :through => :station_connections,
-           source: :connected_station, dependent: :destroy
+           source: :connected_station
 
   has_many :station_order_numbers, dependent: :destroy
   has_many :routes, :through => :station_order_numbers
@@ -9,4 +11,10 @@ class Station < ApplicationRecord
   has_many :passing_trains, dependent: :destroy
 
   validates :name, presence: true, length: {maximum: 30, minimum: 1}
+
+  def destroy_connections
+    StationConnection.where(station_id: self.id)
+                     .or(StationConnection.where(connected_station_id: self.id))
+                     .destroy_all
+  end
 end
