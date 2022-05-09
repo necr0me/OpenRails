497516d6b5c31658b2c27660cf63e83c89610a2e
pavel <ortusprogressor@gmail.com>
2022-05-09 12:25:22 +0300

    Added tooltip to taken seats (admin can see who took that seat).

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   has_one :user_info, dependent: :destroy
   has_many :tickets, dependent: :destroy
   accepts_nested_attributes_for(:user_info, allow_destroy: true)
+
+  def initials
+    self.user_info.present? ? "#{self.user_info.surname} #{self.user_info.name} #{self.user_info.patronymic}" : "user_id: #{self.id}"
+  end
 end
